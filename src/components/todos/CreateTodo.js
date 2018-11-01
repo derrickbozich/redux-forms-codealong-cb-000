7786d3173c344b2a1105c5b85fff6a1de6b8d40e
derrick b <dbozich1@gmail.com>
2018-11-01 01:35:53 +0000

    automatically backed up by learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
- 
+
 class CreateTodo extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  };
   render(){
     return(
       <div>
@@ -16,5 +29,5 @@ class CreateTodo extends Component {
     );
   }
 };
- 
+
 export default CreateTodo;
